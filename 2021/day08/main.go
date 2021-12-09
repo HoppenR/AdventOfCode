8c23e32b5a7f181a33b8c89d6e8d9569b63c64a5
Christoffer Lundell <christofferlundell@protonmail.com>
2021-12-09 13:12:03 +0100

    AoC2021#8: Clean Up and make more readable

--- 2021/day08/main.go
+++ 2021/day08/main.go
@@ -36,66 +36,49 @@ func CountUnique(patterns []SignalPattern) (count int) {
 
 func DecodeSignals(patterns []SignalPattern) (ans int) {
 	for _, sp := range patterns {
-		mappings := make([]int, 10)
-		// Uniques:
-		//     '1' is the only one with length 2
-		//     '7' is the only one with length 3
-		//     '4' is the only one with length 4
-		//     '8' is the only one with length 7
-		for i := 0; i < len(sp.in); {
-			switch BitLen(sp.in[i]) {
+		numToSig := make([]int, 10)
+		for _, sig := range sp.in {
+			switch BitLen(sig) {
 			case 2:
-				sp.in = MoveValue(sp.in, mappings, i, 1)
+				numToSig[1] = sig
 			case 3:
-				sp.in = MoveValue(sp.in, mappings, i, 7)
+				numToSig[7] = sig
 			case 4:
-				sp.in = MoveValue(sp.in, mappings, i, 4)
+				numToSig[4] = sig
 			case 7:
-				sp.in = MoveValue(sp.in, mappings, i, 8)
-			default:
-				i++
+				numToSig[8] = sig
 			}
 		}
-		// Length == 6:
-		//     '9' is the only one of which '4' is a perfect subset.
-		//     '6' is the only one of which '1' is _not_ a perfect subset
-		//     '0' is the only one left
-		for i := 0; i < len(sp.in); {
-			if BitLen(sp.in[i]) != 6 {
-				i++
+		for _, sig := range sp.in {
+			if BitLen(sig) != 6 {
 				continue
 			}
-			if Contains(sp.in[i], mappings[4]) {
-				sp.in = MoveValue(sp.in, mappings, i, 9)
-			} else if !Contains(sp.in[i], mappings[1]) {
-				sp.in = MoveValue(sp.in, mappings, i, 6)
+			if Contains(sig, numToSig[4]) {
+				numToSig[9] = sig
+			} else if !Contains(sig, numToSig[1]) {
+				numToSig[6] = sig
 			} else {
-				sp.in = MoveValue(sp.in, mappings, i, 0)
+				numToSig[0] = sig
 			}
 		}
-		// Length == 5:
-		//     '5' is the only one that is a perfect subset of '6'
-		//     '3' is the only one of which '1' is a perfect subset
-		//     '2' is the only one left
-		for i := 0; i < len(sp.in); {
-			if BitLen(sp.in[i]) != 5 {
-				i++
+		for _, sig := range sp.in {
+			if BitLen(sig) != 5 {
 				continue
 			}
-			if Contains(mappings[6], sp.in[i]) {
-				sp.in = MoveValue(sp.in, mappings, i, 5)
-			} else if Contains(sp.in[i], mappings[1]) {
-				sp.in = MoveValue(sp.in, mappings, i, 3)
+			if Contains(numToSig[6], sig) {
+				numToSig[5] = sig
+			} else if Contains(sig, numToSig[1]) {
+				numToSig[3] = sig
 			} else {
-				sp.in = MoveValue(sp.in, mappings, i, 2)
+				numToSig[2] = sig
 			}
 		}
 		decodedNumber := 0
-		for _, ov := range sp.out {
+		for _, output := range sp.out {
 			decodedNumber *= 10
-			for i, v := range mappings {
-				if v == ov {
-					decodedNumber += i
+			for num, signal := range numToSig {
+				if signal == output {
+					decodedNumber += num
 					break
 				}
 			}
@@ -105,17 +88,10 @@ func DecodeSignals(patterns []SignalPattern) (ans int) {
 	return
 }
 
-// Copies val src[srcIndex] → dst[dstIndex], deletes it from src, returns new src
-func MoveValue(src, dst []int, srcIndex, dstIndex int) []int {
-	dst[dstIndex] = src[srcIndex]
-	return append(src[:srcIndex], src[srcIndex+1:]...)
-}
-
 // Returns number of bits set in b
 func BitLen(b int) (count int) {
-	for b > 0 {
-		count += b & 1
-		b >>= 1
+	for n := b; n > 0; n >>= 1 {
+		count += n & 1
 	}
 	return
 }
