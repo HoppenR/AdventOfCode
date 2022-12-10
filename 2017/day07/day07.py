396edd3430fd2f6c7d2c76ab0aa063ad65f10e40
Christoffer Lundell <christofferlundell@protonmail.com>
2022-12-10 20:43:54 +0100

    AoC2017#7: Initial solution

---
+++ 2017/day07/day07.py
@@ -0,0 +1,65 @@
+from argparse import ArgumentParser
+from typing import Dict, List, Tuple
+
+class Disc:
+    name: str
+    weight: int
+    dependencies: List[str]
+    def __init__(self):
+        self.dependencies = []
+
+def delve(discname: str, discs: Dict[str, Disc]) -> Tuple[int, bool]:
+    disc: Disc = discs[discname]
+    weights: Dict[str, int] = {}
+    for child in disc.dependencies:
+        weights[child], has_found = delve(child, discs)
+        if has_found:
+            return weights[child], True
+    last: int = 0
+    for name, weight in sorted(weights.items(), key=lambda x: x[1]):
+        if last == 0:
+            last = weight
+        else:
+            if weight != last:
+                diff: int = weight - last
+                return (discs[name].weight - diff, True)
+    return sum(weights.values()) + disc.weight, False
+
+def find_unbalance(discs: Dict[str, Disc]) -> int:
+    needed, _ = delve(find_top_node(discs), discs)
+    return needed
+
+def find_top_node(discs: Dict[str, Disc]) -> str:
+    needed: Dict[str, bool] = {d.name: False for d in discs.values()}
+    for disc in discs.values():
+        for dep in disc.dependencies:
+            needed[dep] = True
+    for k, v in needed.items():
+        if not v:
+            return k
+    assert(False)
+
+
+def main() -> int:
+    parser = ArgumentParser()
+    parser.add_argument('file')
+    args = parser.parse_args()
+    with open(args.file, encoding='utf-8') as f:
+        discs: Dict[str, Disc] = {}
+        for lineraw in f.readlines():
+            line = lineraw.strip()
+            disc: Disc = Disc()
+            parts = line.split(" -> ")
+            if len(parts) > 1:
+                disc.dependencies = parts[1].split(", ")
+            props = parts[0].split(" ")
+            disc.name = props[0]
+            disc.weight = int(props[1][1:-1], base=10)
+            discs[disc.name] = disc
+
+        print(find_top_node(discs))
+        print(find_unbalance(discs))
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
--- README.md
+++ README.md
@@ -90,7 +90,7 @@ Clicking a star takes you to the solution
   <td><a href=2020/day07><img src="goldstar.png" width=16 height=16></a></td>
   <td><a href=2019/day07><img src="goldstar.png" width=16 height=16></a></td>
   <td><a href=2018/day07><img src="silverstar.png" width=16 height=16></a></td>
-  <td></td>
+  <td><a href=2017/day07><img src="goldstar.png" width=16 height=16></a></td>
   <td></td>
   <td></td>
  </tr>
