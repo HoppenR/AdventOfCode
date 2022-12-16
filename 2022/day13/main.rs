4746f93b211349311f2c2cca5952e5cf82558d7f
Christoffer Lundell <christofferlundell@protonmail.com>
2022-12-16 16:14:52 +0100

    AoC2022#13: Improve solution

--- 2022/day13/main.rs
+++ 2022/day13/main.rs
@@ -2,115 +2,78 @@ use std::cmp;
 use std::io::{self, Error, Read, Write};
 use std::str::Chars;
 
-#[derive(Clone, PartialEq)]
-enum PackT {
-    List,
-    Num,
+#[derive(Clone, Eq, PartialEq)]
+enum Packet {
+    List(Vec<Packet>),
+    Value(u32),
 }
 
-#[derive(Clone)]
-struct Packet {
-    children: Vec<Packet>,
-    t: PackT,
-    value: u32,
-}
-
-impl Packet {
-    fn new_list(children: Vec<Packet>) -> Self {
-        return Packet {
-            children,
-            t: PackT::List,
-            value: 0,
-        };
-    }
-
-    fn new_num(value: u32) -> Self {
-        return Packet {
-            children: Vec::new(),
-            t: PackT::Num,
-            value,
-        };
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        return Some(self.cmp(other));
     }
 }
 
-fn num_to_list(num_packet: &Packet) -> Packet {
-    return Packet::new_list(vec![Packet::new_num(num_packet.value)]);
-}
-
-fn compare_packets(lhs: &Packet, rhs: &Packet) -> cmp::Ordering {
-    if lhs.t == PackT::List && rhs.t == PackT::List {
-        for i in 0..cmp::min(lhs.children.len(), rhs.children.len()) {
-            let comp = compare_packets(&lhs.children[i], &rhs.children[i]);
-            if comp.is_ne() {
-                return comp;
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        match (self, other) {
+            (Packet::List(lhs), Packet::List(rhs)) => {
+                return lhs.cmp(rhs);
+            }
+            (Packet::Value(lhs), Packet::Value(rhs)) => {
+                return lhs.cmp(rhs);
+            }
+            (Packet::Value(lhs), Packet::List(_)) => {
+                return Packet::List(vec![Packet::Value(*lhs)]).cmp(other);
+            }
+            (Packet::List(_), Packet::Value(rhs)) => {
+                return self.cmp(&Packet::List(vec![Packet::Value(*rhs)]));
             }
         }
-        return lhs.children.len().cmp(&rhs.children.len());
-    } else if lhs.t == PackT::Num && rhs.t == PackT::Num {
-        return lhs.value.cmp(&rhs.value);
-    } else if lhs.t == PackT::Num && rhs.t == PackT::List {
-        return compare_packets(&num_to_list(lhs), rhs);
-    } else {
-        return compare_packets(lhs, &num_to_list(rhs));
     }
 }
 
 fn sorted_packets(packets: &Vec<Packet>) -> usize {
-    let mut index_count: usize = 0;
-    for (i, pair) in packets.windows(2).step_by(2).enumerate() {
-        if compare_packets(&pair[0], &pair[1]).is_lt() {
-            index_count += i + 1;
-        }
-    }
-    return index_count;
+    return packets
+        .windows(2)
+        .step_by(2)
+        .enumerate()
+        .filter(|(_, pair)| pair[0] < pair[1])
+        .map(|(i, _)| i + 1)
+        .sum();
 }
 
 fn decoder_key(old_packets: &Vec<Packet>) -> usize {
-    let mut packets = old_packets.clone();
+    let mut packets: Vec<Packet> = old_packets.clone();
     let mut product: usize = 1;
-    packets.push(parse_root_packet(&mut "[[2]]".chars()));
-    packets.push(parse_root_packet(&mut "[[6]]".chars()));
-    packets.sort_by(compare_packets);
+    let divider1: &Packet = &parse_root_packet(&mut "[[2]]".chars());
+    let divider2: &Packet = &parse_root_packet(&mut "[[6]]".chars());
+    packets.push(divider1.clone());
+    packets.push(divider2.clone());
+    packets.sort();
     for (i, packet) in packets.iter().enumerate() {
-        if is_divider_packet(&packet) {
-            product *= i + 1
+        if packet == divider1 || packet == divider2 {
+            product *= i + 1;
         }
     }
     return product;
 }
 
-fn is_divider_packet(root: &Packet) -> bool {
-    let mut packet = root;
-    for _ in 0..2 {
-        if packet.children.len() != 1 {
-            return false;
-        }
-        packet = &packet.children[0];
-    }
-    match packet.value {
-        2 => return true,
-        6 => return true,
-        _ => return false,
-    }
-}
-
 fn parse_packet(ch_iter: &mut Chars) -> Packet {
     let mut number_str: String = String::new();
-    let mut packet: Packet = Packet::new_list(Vec::new());
+    let mut packet: Vec<Packet> = Vec::new();
     loop {
-        let next_c = ch_iter.next().unwrap();
+        let next_c: char = ch_iter.next().unwrap();
         match next_c {
             '[' => {
-                packet.children.push(parse_packet(ch_iter));
+                packet.push(parse_packet(ch_iter));
             }
             ']' | ',' => {
                 if number_str.len() > 0 {
-                    packet
-                        .children
-                        .push(Packet::new_num(number_str.parse().unwrap()));
+                    packet.push(Packet::Value(number_str.parse().unwrap()));
                 }
                 if next_c == ']' {
-                    return packet;
+                    return Packet::List(packet);
                 }
                 number_str.clear();
             }
