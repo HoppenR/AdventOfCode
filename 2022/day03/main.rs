931fc40a1ec53b8ca2fd1571510926a14b65dc59
Christoffer Lundell <christofferlundell@protonmail.com>
2023-06-09 11:37:33 +0200

    cleanup 2016#3

--- 2022/day03/main.rs
+++ 2022/day03/main.rs
@@ -2,21 +2,23 @@ use std::collections::HashSet;
 use std::io::{self, Error, Read, Write};
 use std::iter::FromIterator;
 
+fn char_score(ch: char) -> u32 {
+    if ch.is_ascii_uppercase() {
+        return 27 + (ch as u32 - 'A' as u32);
+    } else {
+        return 01 + (ch as u32 - 'a' as u32);
+    }
+}
+
 fn item_priority_sum(rucksacks: &Vec<&str>) -> u32 {
     return rucksacks
         .iter()
-        .map(|r| r.split_at(r.len() / 2))
-        .map(|(c1, c2)| {
-            return c1
+        .map(|ruck| ruck.split_at(ruck.len() / 2))
+        .map(|(left, right)| {
+            return left
                 .chars()
-                .find(|&c| c2.contains(c))
-                .map(|c| {
-                    if char::is_ascii_uppercase(&c) {
-                        return 27 + (c as u32 - 'A' as u32);
-                    } else {
-                        return 01 + (c as u32 - 'a' as u32);
-                    }
-                })
+                .find(|&ch| right.contains(ch))
+                .map(char_score)
                 .unwrap();
         })
         .sum();
@@ -26,25 +28,18 @@ fn badge_priority_sum(rucksacks: &Vec<&str>) -> u32 {
     return rucksacks
         .chunks(3)
         .map(|group_sacks| {
-            let group_item_sets: Vec<HashSet<char>> = group_sacks
+            return group_sacks
                 .iter()
-                .map(|r| {
-                    return HashSet::from_iter(r.chars());
-                })
-                .collect();
-            return group_item_sets
-                .iter()
-                .skip(1)
-                .fold(group_item_sets[0].clone(), |cur, next| (&cur) & (&next))
-                .into_iter()
-                .next()
-                .map(|c| {
-                    if char::is_ascii_uppercase(&c) {
-                        return 27 + (c as u32 - 'A' as u32);
+                .fold(HashSet::new(), |acc, chset| {
+                    if acc.is_empty() {
+                        return HashSet::from_iter(chset.chars());
                     } else {
-                        return 01 + (c as u32 - 'a' as u32);
+                        return (&acc) & (&HashSet::from_iter(chset.chars()));
                     }
                 })
+                .into_iter()
+                .next()
+                .map(char_score)
                 .unwrap();
         })
         .sum();
