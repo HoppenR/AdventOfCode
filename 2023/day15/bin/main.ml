79509a14b79e6bb8020901b2dc42409037e29e88
Christoffer Lundell <christofferlundell@protonmail.com>
2023-12-17 22:12:18 +0100

    AoC2023#15: Fix function rename

--- 2023/day15/bin/main.ml
+++ 2023/day15/bin/main.ml
@@ -3,7 +3,7 @@ open Printf
 
 let main () : int =
   let init_sequence = In_channel.input_lines stdin |> parse in
-  printf "p1: %d\n" @@ hash_helper init_sequence;
+  printf "p1: %d\n" @@ sum_hashes init_sequence;
   0
 ;;
 
